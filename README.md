80553e60605b468966272b96eeef7833534e6a61
Matt Winter <winter5187@gmail.com>
2021-08-25 09:46:46 -0500

    folders and readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # hw18
+Workout tracker - allows user to view, create, and track daily workouts. 
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
